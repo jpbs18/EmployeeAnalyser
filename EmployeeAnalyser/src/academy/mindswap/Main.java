15c8abec928cfeea8c61dc128a06773774961e42
João Santos <joao.santos@mindswap.academy>
2022-08-08 21:34:32 +0100

    Commited

--- EmployeeAnalyser/src/academy/mindswap/Main.java
+++ EmployeeAnalyser/src/academy/mindswap/Main.java
@@ -6,12 +6,12 @@ import static academy.mindswap.Util.sales;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Analyser.antique(sales, 4));
-        System.out.println(Analyser.aboveSalary(sales, 1200));
-        System.out.println(Analyser.oldestEmployees(sales, 3));
-        System.out.println(Analyser.firstOldest(sales, 25));
-        System.out.println(Analyser.averageSalary(sales));
-        System.out.println(Analyser.commonFirstName(sales, humanResources));
+        System.out.println(Analyser.antique(sales, 4)); // should print 4
+        System.out.println(Analyser.aboveSalary(sales, 1200)); // should print [Jorge Fernandes, Filipa Esteves, Diana Domingues, Guilherme Machado, Vera Couto]
+        System.out.println(Analyser.oldestEmployees(sales, 3)); // should print [Guilherme Machado, Filipa Esteves, Jorge Fernandes]
+        System.out.println(Analyser.firstOldest(sales, 25)); // should print Jorge
+        System.out.println(Analyser.averageSalary(sales)); // should print 1278.0
+        System.out.println(Analyser.commonFirstName(sales, humanResources)); // should print [Filipa, Pedro, David, Guilherme]
 
     }
 
